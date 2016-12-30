c6a8c02963e75f1dc0ab396db7d73bad9984b739
Eduardo Martoni <emartoni@ciandt.com>
2016-12-30 13:35:08 -0200

    Fix environment setup script

--- kroton-vm/scripts/configure-env.sh
+++ kroton-vm/scripts/configure-env.sh
@@ -3,32 +3,33 @@
 # This script configures the environment variables like
 # Drupal code standards and git configuration.
 
-ENV_SETUP_COMPLETE_FILE=/etc/environment_setup_complete2
+ENV_SETUP_COMPLETE_FILE=/etc/environment_setup_complete
 
-# Variables.
+# Variables used to configure the environment.
 CS_PATH="/home/vagrant/.composer/vendor/drupal/coder/coder_sniffer"
 STANDARDS_PATH="/home/vagrant/.composer/vendor/squizlabs/php_codesniffer/CodeSniffer/Standards"
-USER_NAME="Eduardo Martoni"
-USER_MAIL="emartoni@ciandt.com"
+USER_NAME="Full Name"
+USER_MAIL="youmail@ciandt.com"
+VAGRANT_USER="vagrant"
 
 # Check to see if we've already performed this setup.
 if [ ! -e "$ENV_SETUP_COMPLETE_FILE" ]; then
 
   # Update the composer.
-  su - vagrant -c "composer global update drupal/coder --prefer-source"
+  su - $VAGRANT_USER -c "composer global update drupal/coder --prefer-source"
 
   # Add the coder to global path.
   echo 'export PATH="$PATH:/home/vagrant/.composer/vendor/bin"' >> /home/vagrant/.profile
 
   # Register the Drupal and DrupalPractice.
-  su - vagrant -c "phpcs --config-set installed_paths $CS_PATH"
+  su - $VAGRANT_USER -c "phpcs --config-set installed_paths $CS_PATH"
 
   # Git config.
-  su - vagrant -c "git config --global user.name '$USER_NAME'"
-  su - vagrant -c "git config --global user.mail '$USER_MAIL'"
-  su - vagrant -c "git config --global merge.tool meld"
-  su - vagrant -c "git config --global credential.helper cache"
-  su - vagrant -c "git config --global push.default nothing"
+  su - $VAGRANT_USER -c "git config --global user.name '$USER_NAME'"
+  su - $VAGRANT_USER -c "git config --global user.mail '$USER_MAIL'"
+  su - $VAGRANT_USER -c "git config --global merge.tool meld"
+  su - $VAGRANT_USER -c "git config --global credential.helper cache"
+  su - $VAGRANT_USER -c "git config --global push.default nothing"
 
   # Create a file to indicate this script has already run.
   sudo touch $ENV_SETUP_COMPLETE_FILE
